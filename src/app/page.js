06a3ce114c90e3dd7c88fef40c6ee8b8e7cfc88a
lleung030 <lucasleung2593@gmail.com>
2024-01-08 15:59:57 -0800

    implement tictactoe into main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TicTacToe from './components/game/TicTacToe';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello world</h1>
+      <Router>
+        <Routes>
+          <Route path='tictactoe' element={<TicTacToe />} />
+        </Routes>
+      </Router>
     </main>
   )
 }
